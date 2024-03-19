3384b07a3727354b7280a219a56331bdbd506904
Oliwyatt <oliverhenrywyatt06@gmail.com>
2024-03-19 19:45:35 +0000

    Extra line removed

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def Create_User():
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    
     try:
         if request.method == "POST":
             if Current_User.login(Email=request.form["Email"], Password=request.form["Password"]):
